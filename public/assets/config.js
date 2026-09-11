d4b896e9e7563fb93c6387c4a5a35f5242f8bd1b
dn-lx <don.alex.perera@gmail.com>
2026-09-11 11:35:52 +0200

    Force white-circle favicon on calculator and app pages

--- public/assets/config.js
+++ public/assets/config.js
@@ -1,3 +1,15 @@
+function applyFrankiFlowFavicon(){
+  let icon=document.querySelector('link[rel~="icon"]');
+  if(!icon){icon=document.createElement('link');icon.rel='icon';document.head.append(icon)}
+  icon.href='/assets/frankiflow-favicon.svg?v=20260911b';
+  icon.type='image/svg+xml';
+}
+
+if(typeof document!=='undefined'){
+  applyFrankiFlowFavicon();
+  setTimeout(applyFrankiFlowFavicon,0);
+}
+
 export const FRANKIFLOW_CONFIG = Object.freeze({
   supabaseUrl: 'https://bdeajozhylypiidrldka.supabase.co',
   supabasePublishableKey: 'sb_publishable_FX7QKUBZQmwykGcgv8SdfQ_lGRZF8n2',
