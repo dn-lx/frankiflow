cfa8d29c6e94975bdb410e050377eaf0efd9c31f
dn-lx <don.alex.perera@gmail.com>
2026-09-12 12:15:54 +0200

    Remove homepage About teaser and keep dedicated About navigation

--- public/assets/app.js
+++ public/assets/app.js
@@ -1,4 +1,4 @@
 import './app-base.js';
 await import('./app-enhancements.js');
 await import('./homepage-refinements.js');
-await import('./homepage-about-v2.js');
+await import('./homepage-about-nav.js');
