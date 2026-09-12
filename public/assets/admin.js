ef640d388e05a2c9ebe7dcd9c4f5d7f338002e12
dn-lx <don.alex.perera@gmail.com>
2026-09-12 12:24:21 +0200

    Load admin logo size control

--- public/assets/admin.js
+++ public/assets/admin.js
@@ -1,4 +1,5 @@
 import './admin-base.js';
 await import('./admin-enhancements.js');
+await import('./admin-logo-size.js');
 await import('./admin-cms-v2.js');
 await import('./admin-unified-ui.js');
