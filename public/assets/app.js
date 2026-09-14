2449a4ef5522593b6b682598bc9286b6ff48fa78
dn-lx <don.alex.perera@gmail.com>
2026-09-14 02:49:38 +0200

    Load icon-only WhatsApp header action

--- public/assets/app.js
+++ public/assets/app.js
@@ -2,3 +2,4 @@ import './app-base.js';
 await import('./app-enhancements.js');
 await import('./homepage-refinements.js');
 await import('./homepage-about-nav.js');
+await import('./whatsapp-nav-icon.js');
