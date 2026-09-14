b2feba52cb7701a7e82d0ac1b2daebe5198a76ea
dn-lx <don.alex.perera@gmail.com>
2026-09-14 02:54:01 +0200

    Restore original WhatsApp header icon source

--- public/assets/app.js
+++ public/assets/app.js
@@ -2,4 +2,3 @@ import './app-base.js';
 await import('./app-enhancements.js');
 await import('./homepage-refinements.js');
 await import('./homepage-about-nav.js');
-await import('./whatsapp-nav-icon.js');
